78317f3964b779fad418b75bde39462b27187762
alexisechano <alexis.echano@gmail.com>
2021-07-12 10:46:29 -0700

    Set up secret keys and Trakt

---
+++ app/src/main/java/com/example/next_show/data/TraktClient.java
@@ -0,0 +1,14 @@
+package com.example.next_show.data;
+
+public class TraktClient {
+    // constant callback urls
+    private static final String TRENDING_SHOWS = "https://api.trakt.tv/shows/trending"; // GET
+    private static final String POPULAR_SHOWS = "https://api.trakt.tv/shows/popular"; // GET
+    private static final String RETRIEVE_SHOW = "https://api.trakt.tv/shows/"; // GET and + id
+
+    // Empty constructor
+    public TraktClient() {
+
+    }
+}
+
