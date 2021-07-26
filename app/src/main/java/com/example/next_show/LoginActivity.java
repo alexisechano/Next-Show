ece6df388f1e2b21d14d457cdaac0497f8b2325c
alexisechano <alexis.echano@gmail.com>
2021-07-26 10:00:57 -0700

    Added transition and UI improvements

--- app/src/main/java/com/example/next_show/LoginActivity.java
+++ app/src/main/java/com/example/next_show/LoginActivity.java
@@ -4,27 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.next_show.models.User;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import java.io.File;
+
 public class LoginActivity extends AppCompatActivity {
     // view element variables
     private EditText etUsername;
     private EditText etPassword;
+    private ImageView ivLogo;
     private Button btnLogin;
     private Button btnSignup;
 
     // constants
     private static final String TAG = "LoginActivity";
+    private static final String LOGO_URL = "https://i.imgur.com/yBGsdF4.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +50,12 @@ public class LoginActivity extends AppCompatActivity {
     private void setViewElems() {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+        ivLogo = findViewById(R.id.ivLogo);
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignup);
+
+        // add logo image
+        Glide.with(this).load(LOGO_URL).centerInside().into(ivLogo);
 
         // set new on click for button
         btnSignup.setBackgroundColor(Color.parseColor("#d6d3d2"));
